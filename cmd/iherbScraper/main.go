156f00535c7e178b19a471b3682e601042771fd6
Ekaterina <katyachkalovski@gmail.com>
2025-07-02 23:11:00 +0500

    entry point updated

--- cmd/iherbScraper/main.go
+++ cmd/iherbScraper/main.go
@@ -1,13 +1,13 @@
 package main
 
 import (
-	"log"
 	"github.com/joho/godotenv"
 	"github.com/zakharova-e/iherb-scraper-server/internal/catalog/delivery/grpc"
 	"github.com/zakharova-e/iherb-scraper-server/internal/catalog/repository/iherbApi"
 	"github.com/zakharova-e/iherb-scraper-server/internal/catalog/usecase"
 	"github.com/zakharova-e/iherb-scraper-server/internal/config"
 	"github.com/zakharova-e/iherb-scraper-server/internal/server"
+	"log"
 )
 
 func main() {
@@ -15,7 +15,7 @@ func main() {
 		log.Println(err)
 	}
 	config.LoadConfig()
-	catalogRepo := iherbApi.NewIherbApiRepository(nil)
+	catalogRepo := iherbApi.NewIherbApiRepository(nil,nil)
 	catalogUsecase := usecase.NewCatalogUsecase(catalogRepo)
 	grpcHandler := grpc.NewGrpcHandler(catalogUsecase)
 	srv := server.NewIherbGrpcServer(grpcHandler)
